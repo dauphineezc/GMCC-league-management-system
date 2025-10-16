e7bd21e8c3e0e5b9f6bdae0d366a7972bc7b969f
Zoe Dauphinee <zdauphinee@greatermidland.org>
2025-10-16 14:16:07 -0400

    add production domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: { serverActions: { allowedOrigins: ['localhost:3000'] } }
+    experimental: { 
+      serverActions: { 
+        allowedOrigins: ['localhost:3000', '*.vercel.app', '*.vercel.com'] 
+      } 
+    }
   };
   export default nextConfig;
   
